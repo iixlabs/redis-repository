9a08813bd0ec68df9d8ceb2a4139f75ee0063b3e
Ben Flowers <ben.j.flowers@gmail.com>
2013-07-02 09:47:41 +0100

    adding package json and dependencies

--- rakefile.rb
+++ rakefile.rb
@@ -1,9 +1,22 @@
 @run_options = {verbose: Rake.application.options.trace}
 
-task :default => [:git]
+task :default => [:dependencies, :jshint, :test, :git]
+multitask :dependencies => [:ruby_dependencies,:node_dependencies]
 
 task :ruby_dependencies do
 	rake_sh 'bundle install --path gems'
+end
+
+task :node_dependencies do
+	sh 'npm install'
+end
+
+task :jshint do
+	sh 'jshint ./tests ./lib'
+end
+
+task :test do 
+	sh 'mocha ./tests --ui qunit --colors --reporter nyan'
 end
 
 task :git => :ruby_dependencies do 
